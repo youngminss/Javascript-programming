6992fe338ee5831a42d0221e12efe60cad8e3ce1
WI Young Min <youngminieo1005@gmail.com>
2021-06-26 20:45:19 +0900

    Feat : JS시작 ~ JS주석

---
+++ Beginner_log/1_Hello/hello.js
@@ -0,0 +1,6 @@
+var hello = `안녕하세요. 위영민입니다.\n
+  React 개발을 진행해 본 결과, JS 기본기의 중요성을 느꼈습니다.\n
+  기본기를 탄탄히 쌓기위해 스프린트 해보겠습니다 !`;
+
+// body 부분을 찾아, text 를 넣는다.
+document.body.innerText = hello;
---
+++ Beginner_log/4_Comment/index.js
@@ -0,0 +1,7 @@
+// 한줄 주석
+/**
+ * 블록주석
+ * 개발자들 사이의 코딩 관례
+ * @param {String} code, 코드
+ */
+function getName(code) {}
